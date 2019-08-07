5e8ac217eaec1429782717dfe3d79f3905e30e39
dewangsolanki3 <46868194+dewangsolanki3@users.noreply.github.com>
2019-08-07 14:03:18 +0530

    Update Digiclock.jsx

--- src/components/Digiclock.jsx
+++ src/components/Digiclock.jsx
@@ -136,6 +136,12 @@ class Clock extends Component {
      </div>
 
     
+        <div className="clock">
+<div className="hr" style={{ transform: `rotateZ(${((h % 12) * unit12 + (hourunit))}deg)` }} />
+<div className="min" style={{ transform: `rotateZ(${(m * unit60) + (minuteunit)}deg)` }} />
+<div className="sec" style={{ transform: `rotateZ(${(s * unit60) + (hourunit + minuteunit)}deg)` }} />
+      </div>
+
 
      </>
    );
